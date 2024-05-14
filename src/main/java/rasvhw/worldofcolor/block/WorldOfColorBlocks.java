ff5a19e78204a9ae0c418590ccb00acd71d2c30c
rasvhw <zyleikewolf135@gmail.com>
2024-05-14 14:11:57 -0500

    begin painted glass

--- src/main/java/rasvhw/worldofcolor/block/WorldOfColorBlocks.java
+++ src/main/java/rasvhw/worldofcolor/block/WorldOfColorBlocks.java
@@ -1,8 +1,15 @@
 package rasvhw.worldofcolor.block;
 
+import net.minecraft.core.block.BlockGlass;
+import net.minecraft.core.block.BlockTrapDoor;
+
 import static org.apache.log4j.builders.appender.SocketAppenderBuilder.LOGGER;
 
 public class WorldOfColorBlocks {
+
+	public static BlockGlass glassPainted;
+	public static BlockTrapDoor trapdoorGlassPainted;
+
 
 	public void initializeBlocks(){
 		LOGGER.info("WorldOfColorBlocks initialized");
