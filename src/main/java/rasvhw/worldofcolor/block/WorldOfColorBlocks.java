14ad704c547559439142a7ca619787d3200c00a3
rasvhw <zyleikewolf135@gmail.com>
2024-05-15 14:11:26 -0500

    Finished Painted Glass. Started Painted Glass Trapdoors

--- src/main/java/rasvhw/worldofcolor/block/WorldOfColorBlocks.java
+++ src/main/java/rasvhw/worldofcolor/block/WorldOfColorBlocks.java
@@ -26,66 +26,128 @@ public class WorldOfColorBlocks {
 	public static Block glassGreenPainted;
 	public static Block glassLimePainted;
 	public static Block glassLightBluePainted;
+	public static Block glassCyanPainted;
 	public static Block glassBluePainted;
 	public static Block glassPurplePainted;
 	public static Block glassMagentaPainted;
 	public static Block glassPinkPainted;
 
-
-	public static Block trapdoorGlassPainted;
+	public static Block trapdoorGlassWhitePainted;
+	public static Block trapdoorGlassSilverPainted;
+	public static Block trapdoorGlassGrayPainted;
+	public static Block trapdoorGlassBlackPainted;
+	public static Block trapdoorGlassRedPainted;
+	public static Block trapdoorGlassBrownPainted;
+	public static Block trapdoorGlassOrangePainted;
+	public static Block trapdoorGlassYellowPainted;
+	public static Block trapdoorGlassGreenPainted;
+	public static Block trapdoorGlassLimePainted;
+	public static Block trapdoorGlassLightBluePainted;
+	public static Block trapdoorGlassCyanPainted;
+	public static Block trapdoorGlassBluePainted;
+	public static Block trapdoorGlassPurplePainted;
+	public static Block trapdoorGlassMagentaPainted;
+	public static Block trapdoorGlassPinkPainted;
 
 
 	public void initializeBlocks(){
 		glassWhitePainted = new BlockBuilder(MOD_ID)
 			.setTextures("glass_painted_white.png")
-			.build(new BlockGlass("glass.white.painted",2800,Material.glass,false));
+			.build(new BlockGlass("glass.white.painted", 2800, Material.glass, false));
 		glassSilverPainted = new BlockBuilder(MOD_ID)
 			.setTextures("glass_painted_silver.png")
-			.build(new BlockGlass("glass.silver.painted",2801,Material.glass,false));
-		glassGrayPainted=new BlockBuilder(MOD_ID)
+			.build(new BlockGlass("glass.silver.painted", 2801, Material.glass, false));
+		glassGrayPainted = new BlockBuilder(MOD_ID)
 			.setTextures("glass_painted_gray.png")
-			.build(new BlockGlass("glass.gray.painted",2802,Material.glass,false));
-		glassBlackPainted=new BlockBuilder(MOD_ID)
+			.build(new BlockGlass("glass.gray.painted", 2802, Material.glass, false));
+		glassBlackPainted = new BlockBuilder(MOD_ID)
 			.setTextures("glass_painted_black.png")
-			.build(new BlockGlass("glass.black.painted",2803,Material.glass,false));
-		glassRedPainted=new BlockBuilder(MOD_ID)
+			.build(new BlockGlass("glass.black.painted", 2803, Material.glass, false));
+		glassRedPainted = new BlockBuilder(MOD_ID)
 			.setTextures("glass_painted_red.png")
-			.build(new BlockGlass("glass.red.painted",2804,Material.glass,false));
-		glassBrownPainted=new BlockBuilder(MOD_ID)
+			.build(new BlockGlass("glass.red.painted", 2804, Material.glass, false));
+		glassBrownPainted = new BlockBuilder(MOD_ID)
 			.setTextures("glass_painted_brown.png")
-			.build(new BlockGlass("glass.brown.painted",2805,Material.glass,false));
-		glassOrangePainted=new BlockBuilder(MOD_ID)
+			.build(new BlockGlass("glass.brown.painted", 2805, Material.glass, false));
+		glassOrangePainted = new BlockBuilder(MOD_ID)
 			.setTextures("glass_painted_orange.png")
-			.build(new BlockGlass("glass.orange.painted",2806,Material.glass,false));
-		glassYellowPainted=new BlockBuilder(MOD_ID)
+			.build(new BlockGlass("glass.orange.painted", 2806, Material.glass, false));
+		glassYellowPainted = new BlockBuilder(MOD_ID)
 			.setTextures("glass_painted_yellow.png")
-			.build(new BlockGlass("glass.yellow.painted",2807,Material.glass,false));
-		glassOrangePainted=new BlockBuilder(MOD_ID)
-			.setTextures("glass_painted_orange.png")
-			.build(new BlockGlass("glass.orange.painted",2808,Material.glass,false));
-		glassOrangePainted=new BlockBuilder(MOD_ID)
-			.setTextures("glass_painted_orange.png")
-			.build(new BlockGlass("glass.orange.painted",2809,Material.glass,false));
-		glassOrangePainted=new BlockBuilder(MOD_ID)
-			.setTextures("glass_painted_orange.png")
-			.build(new BlockGlass("glass.orange.painted",2810,Material.glass,false));
-		glassOrangePainted=new BlockBuilder(MOD_ID)
-			.setTextures("glass_painted_orange.png")
-			.build(new BlockGlass("glass.orange.painted",2811,Material.glass,false));
-		glassOrangePainted=new BlockBuilder(MOD_ID)
-			.setTextures("glass_painted_orange.png")
-			.build(new BlockGlass("glass.orange.painted",2812,Material.glass,false));
-		glassOrangePainted=new BlockBuilder(MOD_ID)
-			.setTextures("glass_painted_orange.png")
-			.build(new BlockGlass("glass.orange.painted",2813,Material.glass,false));
-		glassOrangePainted=new BlockBuilder(MOD_ID)
-			.setTextures("glass_painted_orange.png")
-			.build(new BlockGlass("glass.orange.painted",2814,Material.glass,false));
-		glassOrangePainted=new BlockBuilder(MOD_ID)
-			.setTextures("glass_painted_orange.png")
-			.build(new BlockGlass("glass.orange.painted",2815,Material.glass,false));
+			.build(new BlockGlass("glass.yellow.painted", 2807, Material.glass, false));
+		glassGreenPainted = new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_green.png")
+			.build(new BlockGlass("glass.green.painted", 2808, Material.glass, false));
+		glassLimePainted = new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_lime.png")
+			.build(new BlockGlass("glass.lime.painted", 2809, Material.glass, false));
+		glassLightBluePainted = new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_lightblue.png")
+			.build(new BlockGlass("glass.light.blue.painted", 2810, Material.glass, false));
+		glassCyanPainted = new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_cyan.png")
+			.build(new BlockGlass("glass.cyan.painted", 2811, Material.glass, false));
+		glassBluePainted = new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_blue.png")
+			.build(new BlockGlass("glass.blue.painted", 2812, Material.glass, false));
+		glassPurplePainted = new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_purple.png")
+			.build(new BlockGlass("glass.purple.painted", 2813, Material.glass, false));
+		glassMagentaPainted = new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_magenta.png")
+			.build(new BlockGlass("glass.magenta.painted", 2814, Material.glass, false));
+		glassPinkPainted = new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_pink.png")
+			.build(new BlockGlass("glass.pink.painted", 2815, Material.glass, false));
 
-
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.white.painted",2816,Material.glass,false));
+		trapdoorGlassSilverPainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_silver.png")
+			.build(new BlockTrapDoor("trapdoor.glass.silver.painted",2817,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.gray.painted",2818,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.black.painted",2819,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.red.painted",2820,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.brown.painted",2821,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.orange.painted",2822,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.yellow.painted",2823,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.green.painted",2824,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.lime.painted",2825,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.light.blue.painted",2826,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.cyan.painted",2827,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.blue.painted",2828,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.purple.painted",2829,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.magenta.painted",2830,Material.glass,false));
+		trapdoorGlassWhitePainted=new BlockBuilder(MOD_ID)
+			.setTextures("glass_painted_white.png")
+			.build(new BlockTrapDoor("trapdoor.glass.pink.painted",2831,Material.glass,false));
 
 		LOGGER.info("WorldOfColorBlocks initialized");
 	}
--- src/main/java/rasvhw/worldofcolor/recipes/WorldOfColorRecipes.java
+++ src/main/java/rasvhw/worldofcolor/recipes/WorldOfColorRecipes.java
@@ -1,13 +1,104 @@
 package rasvhw.worldofcolor.recipes;
 
+import net.minecraft.core.block.Block;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
+import turniplabs.halplibe.helper.RecipeBuilder;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 import static org.apache.log4j.builders.appender.SocketAppenderBuilder.LOGGER;
+import static rasvhw.worldofcolor.WorldOfColor.MOD_ID;
+import static rasvhw.worldofcolor.block.WorldOfColorBlocks.*;
 
 public class WorldOfColorRecipes implements RecipeEntrypoint {
 	@Override
 	public void onRecipesReady() {
+		//RecipeBuilder.Shaped(MOD_ID)
+		//			.setShape("1", "2")
+		//			.addInput('1', Block.blockClay)
+		//			.addInput('2', Block.sand)
+		//			.create("grey_soldier", new ItemStack(greyDoll, 4));
 
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,0))
+			.create("black_stained_glass",new ItemStack(glassBlackPainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,1))
+			.create("red_stained_glass",new ItemStack(glassRedPainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,2))
+			.create("green_stained_glass",new ItemStack(glassGreenPainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,3))
+			.create("brown_stained_glass",new ItemStack(glassBrownPainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,4))
+			.create("blue_stained_glass",new ItemStack(glassBluePainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,5))
+			.create("purple_stained_glass",new ItemStack(glassPurplePainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,6))
+			.create("cyan_stained_glass",new ItemStack(glassCyanPainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,7))
+			.create("light_gray_stained_glass",new ItemStack(glassSilverPainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,8))
+			.create("gray_stained_glass",new ItemStack(glassGrayPainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,9))
+			.create("pink_stained_glass",new ItemStack(glassPinkPainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,10))
+			.create("lime_stained_glass",new ItemStack(glassLimePainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,11))
+			.create("yellow_stained_glass",new ItemStack(glassYellowPainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,12))
+			.create("light_blue_stained_glass",new ItemStack(glassLightBluePainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,13))
+			.create("magenta_stained_glass",new ItemStack(glassMagentaPainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,14))
+			.create("orange_stained_glass",new ItemStack(glassOrangePainted,8));
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape("111","121","111")
+			.addInput('1', Block.glass)
+			.addInput('2', new ItemStack(Item.dye,1,15))
+			.create("white_stained_glass",new ItemStack(glassWhitePainted,8));
 	}
 
 	@Override
